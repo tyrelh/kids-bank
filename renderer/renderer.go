684e58d940f4b8632486ce1cabf62f7c4ebdd7b1
Tyrel Delaney <tyrel.hiebert@gmail.com>
2025-03-08 15:12:04 -0800

    embed html templates

--- renderer/renderer.go
+++ renderer/renderer.go
@@ -1,13 +1,22 @@
 package renderer
 
 import (
+	"embed"
 	"net/http"
 	"text/template"
 )
 
+//go:embed *.html
+var templates embed.FS
+
 func RenderIndex(w http.ResponseWriter, r *http.Request) {
-	templ := template.Must(template.ParseFiles("index.html"))
-	err := templ.Execute(w, nil)
+	templ, err := template.ParseFS(templates, "index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = templ.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
